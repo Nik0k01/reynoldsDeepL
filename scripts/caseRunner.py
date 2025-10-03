cdc38f79e03e30f14957591c9856349d7f1a56eb
Nikodem Dabrowski <asapnik0k0@gmail.com>
2025-10-03 15:56:35 +0200

    Updated path

--- scripts/caseRunner.py
+++ scripts/caseRunner.py
@@ -1,14 +1,15 @@
-# Import necessary library to run multiple processes at the same time
 from concurrent.futures import ProcessPoolExecutor, as_completed
 import os
+import pathlib
 from PyFoam.Execution.BasicRunner import BasicRunner
 import re
 
 # Pattern to match numbers with exactly 2 decimal places
 pattern = r'\d+\.\d{2}'
 # Directory containing the flow cases
+path = pathlib.Path.cwd()
 case_directory = "flowCases/"
-case_list = os.listdir(case_directory)
+case_list = os.listdir((path / case_directory).as_posix())
 
 def solve_flow(case_name):
     # Wrapper for BasicRunner - solving the case
@@ -29,7 +30,11 @@ def solve_flow(case_name):
 
 # Running multiple cases at once to save time
 with ProcessPoolExecutor(max_workers=6) as executor:
-    futures = [executor.submit(solve_flow, case_name = os.path.join(case_directory, case)) for case in case_list]
+    futures = [
+        executor.submit(solve_flow, case_name=(path / case_directory / case).as_posix()) 
+        for case in case_list
+        ]
     for f in as_completed(futures):
         Re, case_name, ok, msg = f.result()
         print(f"{case_name} [Re={Re}] -> {msg}, success={ok}")
+        
